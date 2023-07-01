f826463a55b2c9efc23754b711f19ff02dcaf074
hailan <phr9000@gmail.com>
2023-07-01 23:19:15 +0900

    스타일 시트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import './styles/index.scss'
+import * as antd from 'antd'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="test-container">
+        <p>
           Edit <code>src/App.tsx</code> and save to reload11.
+          <antd.Button type="primary">버튼</antd.Button>
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
