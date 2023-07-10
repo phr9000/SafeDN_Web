8a4349ecc6398649b542153fb82a9d08389dee78
hailan <phr9000@gmail.com>
2023-07-10 18:02:19 +0900

    타일윈드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import './assets/styles/index.scss'
-import { Button } from 'antd'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload11.
-          <Button type="primary">버튼</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="text-3xl font-bold underline">test</div>
       </header>
     </div>
   )
